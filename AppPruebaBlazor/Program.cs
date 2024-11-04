4abb71f2dfe34e71be8515817cbe741ebd960c7e
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-04 12:03:11 -0300

    Init project and install telerik

---
+++ AppPruebaBlazor/Program.cs
@@ -0,0 +1,14 @@
+using AppPruebaBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register the Telerik services.
+builder.Services.AddTelerikBlazor();
+
+await builder.Build().RunAsync();
