0f413cb70c331df6922bba3607dd6c44ff082a6c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-08 21:11:35 -0300

    Creo el archivo AgregarEquipo.razor.cs al proyecto

---
+++ Pages/AgregarEquipo.razor.cs
@@ -0,0 +1,14 @@
+﻿using AppPruebaBlazor.Models;
+
+namespace AppPruebaBlazor.Pages
+{
+    public partial class AgregarEquipo
+    {
+        private Equipo nuevoEquipo = new Equipo();
+        private async Task AgregarEquipoNuevo() 
+        {
+            await EquipoService.AgregarEquipoAsync(nuevoEquipo);
+            NavigationManager.NavigateTo("/equipos");
+        }
+    }
+}
