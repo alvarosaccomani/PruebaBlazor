de02fc979854d5555cda2863f648c3289b3c2ba2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-11 09:23:13 -0300

    Creo el archivo EditarEquipo.razor.cs al proyecto

---
+++ Pages/EditarEquipo.razor.cs
@@ -0,0 +1,19 @@
+﻿using AppPruebaBlazor.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AppPruebaBlazor.Pages
+{
+    public partial class EditarEquipo
+    {
+        [Parameter] 
+        public int eq_Id { get; set; } 
+        private Equipo equipo = new Equipo(); 
+        protected override async Task OnInitializedAsync() { 
+            equipo = await EquipoService.ObtenerEquipoPorIdAsync(eq_Id);
+        }
+        private async Task ActualizarEquipo() { 
+            await EquipoService.ActualizarEquipoAsync(equipo); 
+            NavigationManager.NavigateTo("/equipos"); 
+        }
+    }
+}
