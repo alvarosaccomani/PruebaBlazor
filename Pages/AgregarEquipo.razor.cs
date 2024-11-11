5f5a179056dc3b30f2558747d04f6b3702b9c656
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-11 14:04:55 -0300

    Agrego el campo eq_Documentacion y eq_Contenido al componente AgregarEquipo

--- Pages/AgregarEquipo.razor.cs
+++ Pages/AgregarEquipo.razor.cs
@@ -1,14 +1,35 @@
 ﻿using AppPruebaBlazor.Models;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace AppPruebaBlazor.Pages
 {
     public partial class AgregarEquipo
     {
         private Equipo nuevoEquipo = new Equipo();
+        private IBrowserFile archivoPdf;
+
+        private async Task SeleccionarArchivo(InputFileChangeEventArgs e) {
+            archivoPdf = e.File;
+        }
         private async Task AgregarEquipoNuevo() 
         {
-            await EquipoService.AgregarEquipoAsync(nuevoEquipo);
-            NavigationManager.NavigateTo("/equipos");
+            try
+            {
+                using var ms = new MemoryStream();
+                if (archivoPdf != null)
+                {
+                    await archivoPdf.OpenReadStream(50000000).CopyToAsync(ms); // Limitar a 50 MB
+                    nuevoEquipo.eq_Documentacion = archivoPdf.Name;
+                    nuevoEquipo.eq_Contenido = ms.ToArray();
+                }
+
+                await EquipoService.AgregarEquipoAsync(nuevoEquipo);
+                NavigationManager.NavigateTo("/equipos");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
