deb12cb3994d1d6e86d4688922c17af8172a73e2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2024-11-11 10:12:54 -0300

    Creo el archivo EliminarEquipo.razor.cs en el proyecto

---
+++ Pages/EliminarEquipo.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AppPruebaBlazor.Pages
+{
+    public partial class EliminarEquipo
+    {
+        [Parameter]
+        public int eq_Id { get; set; }
+        
+        private async Task BorrarEquipo() {        
+            await EquipoService.EliminarEquipoAsync(eq_Id);
+            NavigationManager.NavigateTo("/equipos");
+        }
+        
+        private void Cancelar() {
+            NavigationManager.NavigateTo("/equipos");
+        }
+    }
+}
